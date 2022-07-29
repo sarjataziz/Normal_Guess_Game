f24c5601c560fc326fdd4d9f5b4d20618a9d28d4
sarjataziz <sarjat.aziz@gmail.com>
2022-07-29 21:44:03 +0600

    first commit

---
+++ v1.py
@@ -0,0 +1,15 @@
+import random
+
+random_number = random.randint(1, 10)        # number 1-10
+
+guess = None
+while guess != random_number:
+    guess = int(input("Pick a number from 1-10 : "))
+
+    if guess < random_number:
+        print("TOO LOW!")
+    elif guess > random_number:
+        print("TOO HIGH!")
+    else:
+        print("YOU WIN!!!!!!!")
+print(random_number)
---
+++ v2.py
@@ -0,0 +1,22 @@
+import random
+
+random_number = random.randint(1,10)    # number 1-10 
+
+while True:
+	guess = int(input("Pick a number from 1-10 : "))
+
+	if guess < random_number:
+		print("TOO LOW!")
+	elif guess > random_number:
+		print("TOO HIGH!")
+	else:
+		print("YOU WIN!!!!!!!")
+
+		play_again = input("Do you want to play again? (y/n) : ")
+		if play_again == "y":
+			random_number = random.randint(1,10)
+			guess = None
+		else:
+			print("Thanks for playing......")
+			break
+			
